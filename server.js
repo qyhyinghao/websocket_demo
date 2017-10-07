ae7a680aa6e3692c748b38c39fa3fd5a21754cf5
zhengwei1949 <zhengwei1949@qq.com>
2017-10-07 11:44:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log('接收到客户端发送的消息', message);
+    wx.send('你发送的消息是'+message);
   });
 
-  ws.send('something');
+  ws.send('你好，有什么可以帮助您的');
 });
